52d98f5ea05892815397666cae59849741acb84e
Pierre Neidhardt <ambrevar@gmail.com>
2015-09-09 18:18:52 +0200

    push.sh: Upload helper script

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ To add a PKGBUILD from an existing reposity (AUR >= 4):
 
 	git subtree add -P ${name} ssh://aur@aur.archlinux.org/${name}.git master
 
-To push to the AUR:
-
-	git subtree push -P ${name} ssh://aur@aur.archlinux.org/${name}.git master
+Use `push.sh` to push to the AUR.
 
 See `import.sh` for a helper script that will upload AUR4 PKGBUILD from AUR3 and
 save the corresponding subtree here.
---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+	cat<<EOF
+Usage: ${0##*/} PACKAGE
+
+Make sure .SRCINFO is up to date and push PACKAGE to the AUR.
+EOF
+	exit
+fi	
+
+pkg="${1##*/}"
+[ ! -d "$pkg" ] && exit
+cd "$pkg"
+mksrcinfo || exit
+if [ $(git status --porcelain | grep -ce "^?? $pkg") -ne 0 ]; then
+	echo >&2 "$1 has staged changes."
+	exit
+fi
+cd ..
+git subtree push -P "$pkg" ssh://aur@aur.archlinux.org/"$pkg".git master
